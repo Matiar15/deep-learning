f50732ddb6f5bbc16573df880023f1bc40530363
matiar <110628257+Matiar15@users.noreply.github.com>
2025-10-25 19:23:06 +0200

    Update `HyperParameters`: adjust default `epochs` and add `seed` attribute

--- src/hyper_parameters.py
+++ src/hyper_parameters.py
@@ -3,4 +3,5 @@ import pydantic
 class HyperParameters(pydantic.BaseModel):
     learning_rate: float = 0.001
     batch_size: int = 128
-    epochs: int = 100
+    epochs: int = 10
+    seed: int = 42
