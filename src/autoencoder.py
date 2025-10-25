f34a0e6457a26b3df8d660a3255121f5e0d2aade
matiar <110628257+Matiar15@users.noreply.github.com>
2025-10-25 19:23:43 +0200

    Update `AutoEncoder` architecture: add BatchNorm, Dropout, and latent space customization

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -3,34 +3,48 @@ import torch.nn as nn
 PICTURE_SIZE = 28 * 28
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_space = 32):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(PICTURE_SIZE, 128),
+            nn.Linear(PICTURE_SIZE, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Linear(128, 64),
+            nn.Dropout(0.2),
+
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Linear(64, 36),
+            nn.Dropout(0.2),
+
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Linear(36, 18),
-            nn.ReLU(),
-            nn.Linear(18, 9),
+
+            nn.Linear(128, latent_space),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(9, 18),
+            nn.Linear(latent_space, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Linear(18, 36),
+
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Linear(36, 64),
+            nn.Dropout(0.2),
+
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Linear(64, 128),
-            nn.ReLU(),
-            nn.Linear(128, PICTURE_SIZE),
+            nn.Dropout(0.2),
+
+            nn.Linear(512, PICTURE_SIZE),
             nn.Sigmoid(),
         )
 
 
     def forward(self, x):
+        x = x.view(x.size(0), -1)
+
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
