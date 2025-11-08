13b225c1a79bb8ee7a8db53e9b17a1cef7e80f65
matiar <110628257+Matiar15@users.noreply.github.com>
2025-11-08 20:11:22 +0100

    small fixes in training loop

--- src/utils/train_loop.py
+++ src/utils/train_loop.py
@@ -34,7 +34,7 @@ def train_loop_ae(model, train_loader, optimizer, criterion, device):
     return avg_loss, original_image, reconstructed_image
 
 
-def train_loop_vae(model, train_loader, optimizer, criterion, device):
+def train_loop_vae(model, train_loader, optimizer, criterion, device, beta=0.01):
     model.train()
     total_loss = 0.0
     total_samples = 0
@@ -47,22 +47,19 @@ def train_loop_vae(model, train_loader, optimizer, criterion, device):
         optimizer.zero_grad()
         outputs, mean, log_var = model(noisy_images)
 
-        # Flatten target images to match the output shape
-        # images_flat = images.view(images.size(0), -1)
-
-        kl_divergence = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-        beta = 0.1
-        curr_loss = (criterion(outputs, images) + beta) * kl_divergence
+        kl_divergence = -0.5 * torch.mean(torch.sum(1 + log_var - mean.pow(2) - log_var.exp(), dim=1))
+        recon_loss = criterion(outputs, images)
+        curr_loss = recon_loss + beta * kl_divergence
 
         curr_loss.backward()
         optimizer.step()
 
-        total_loss += curr_loss.item() * images.size(0)  # mnoży przez rozmiar batcha
+        total_loss += curr_loss.item() * images.size(0)
         total_samples += images.size(0)
 
         if idx == 0:
             original_image = images[0]
-            reconstructed_image = outputs[0]  # .view(original_image.size())
+            reconstructed_image = outputs[0]
 
     avg_loss = total_loss / total_samples
 
