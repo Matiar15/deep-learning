98aa0f32e7b450d2a59aa89b7bc152cfb757d976
matiar <110628257+Matiar15@users.noreply.github.com>
2025-11-08 19:30:38 +0100

    change function names, add new method for vae training

--- src/utils/test_model.py
+++ src/utils/test_model.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def test_model_on(model, dataloader, criterion, device) -> float:
+def ae_test_model_on(model, dataloader, criterion, device) -> float:
     model.eval()
     total_loss = 0.0
     total_samples = 0
@@ -24,3 +24,28 @@ def test_model_on(model, dataloader, criterion, device) -> float:
         avg_loss = total_loss / total_samples
 
         return avg_loss
+
+def vae_test_model_on(model, dataloader, criterion, device) -> float:
+    model.eval()
+    total_loss = 0.0
+    total_samples = 0
+
+    with torch.no_grad():
+        for images, noisy_images, _ in dataloader:
+            images, noisy_images = images.to(device), noisy_images.to(device)
+
+            outputs, _, _ = model(noisy_images)
+
+            # Flatten target images to match the output shape
+            # images_flat = images.view(images.size(0), -1)
+
+            curr_loss = criterion(outputs, images)
+
+            # Accumulate loss properly weighted by batch size
+            total_loss += curr_loss.item() * images.size(0)
+            total_samples += images.size(0)
+
+        avg_loss = total_loss / total_samples
+
+        return avg_loss
+
